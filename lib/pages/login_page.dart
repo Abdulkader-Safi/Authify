78eeddeae0a06091ed9e99ddc4e7a97056373ed9
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-19 17:19:22 +0300

    UI is Done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,20 +18,33 @@ class LoginPage extends StatelessWidget {
       backgroundColor: _primaryColor,
       body: Align(
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _avatarWidget(),
-          ],
+        child: Container(
+          width: _deviceWidth,
+          height: _deviceHeight * 0.6,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _avatarWidget(),
+              SizedBox(
+                height: _deviceHeight * 0.05,
+              ),
+              _emailTextField(),
+              _passwordTextField(),
+              SizedBox(
+                height: _deviceHeight * 0.1,
+              ),
+              _loginButton(),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _avatarWidget() {
-    double? circleD = _deviceHeight * 0.25;
+    double circleD = _deviceHeight * 0.25;
     return Container(
       height: circleD,
       width: circleD,
@@ -40,6 +53,77 @@ class LoginPage extends StatelessWidget {
         borderRadius: BorderRadius.circular(500),
         image: const DecorationImage(
           image: AssetImage('assets/images/main_avatar.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: const TextField(
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          hintText: 'Exmailple@gmail.com',
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: const TextField(
+        obscureText: true,
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          hintText: '********',
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return MaterialButton(
+      minWidth: _deviceWidth * 0.38,
+      height: _deviceHeight * 0.055,
+      color: Colors.white,
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+          side: const BorderSide(color: Colors.black)),
+      child: const Text(
+        "Login",
+        style: TextStyle(
+          fontSize: 16,
+          color: _primaryColor,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
